f2e0b472722740773710dac0c09b63b27f493395
Juleanna <juleannadafna@gmail.com>
2026-03-16 09:45:59 +0200

    Видалено невикористаний імпорт django.conf.settings з conftest.py

--- inventory_project/conftest.py
+++ inventory_project/conftest.py
@@ -1,9 +1,8 @@
+import os
+
 import django
-from django.conf import settings
 
 
 def pytest_configure():
-    import os
-
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inventory_project.settings")
     django.setup()
